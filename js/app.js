7c6d4666ac14e15eea38758e5925a5dbae3d9225
Alexander Adam <alexadam@gmail.com>
2015-06-15 20:32:46 -0400

    Re-establishing the project and starting (kinda) fresh

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-var myApp = angular.module('myApp', ['ngRoute', 'firebase', 'appControllers']).constant('FIREBASE_URL','https://carlog.firebaseio.com/');
-
-var appControllers = angular.module('appControllers', ['firebase']);
-
-myApp.config(['$routeProvider', function($routeProvider) {
+var carLogApp = angular.module('carLogApp', ['ngRoute',
+									 'firebase',
+									 'appControllers']).constant('FIREBASE_URL', 'https://carlog.firebaseio.com/');
+									 
+									 
+ carLogApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 		when('/login', {
 			templateUrl: 'views/login.html',
@@ -12,11 +13,24 @@ myApp.config(['$routeProvider', function($routeProvider) {
 			templateUrl: 'views/register.html',
 			controller: 'RegistrationController'
 		}).
-		when('/garage', {
-			templateUrl: 'views/garage.html',
-			controller: 'GarageController'
+		when('/checkins/:uId/:mId', {
+			templateUrl: 'views/checkins.html',
+			controller: 'CheckInsController'
+		}).
+		when('/checkins/:uId/:mId/checkinsList', {
+			templateUrl: 'views/checkinslist.html',
+			controller: 'CheckInsController'
+		}).
+		when('/meetings', {
+			templateUrl: 'views/meetings.html',
+			controller: 'MeetingsController',
+			resolve : {
+				currentAuth : function(Authentication){
+					return Authentication.requireAuth();
+				}
+			}  
 		}).
 		otherwise({
-			redirectTo: '/login'
-	});
+			redirectTo: '/'
+		});
 }]);
