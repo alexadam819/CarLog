5194a49cc122d5863fd5f3fff914ceb8b370e640
Alexander Adam <alexadam@gmail.com>
2015-06-16 22:57:50 -0400

    Setting up the login and registration

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,9 +1,32 @@
 carLogApp.controller('RegistrationController',
-	function ($scope, $firebaseAuth, $location, FIREBASE_URL) {
+	function ($scope, $firebaseAuth, $location, Authentication, FIREBASE_URL) {
 
-		$scope.reg = function(){
-			console.log("here");
-			$('#loginReg').attr("ng-include","'views/register.html'");
-		};
+//		$scope.reg = function(){
+//			console.log("here");
+//			$('#loginReg').attr("ng-include","'views/register.html'");
+//		};
+		
+		var ref = new Firebase(FIREBASE_URL);
+		var auth = $firebaseAuth(ref);
+		
+		$scope.login = function(){
+			console.log($scope.user.email);
+			Authentication.login($scope.user)
+			.then(function(user) {
+				$location.path('garage');
+			}).catch(function(error){
+				$scope.message = error.message;
+			});
+		};//Login
+		
+		$scope.register = function(){
+			Authentication.register($scope.user)
+			.function(function(user){
+				Authentication.login($scope.user);
+				$location.path('/garage');
+			}).catch(function(error){
+				$scope.message = error.toString();
+			});
+		};//Register
 		
 });
---
+++ js/services/authentication.js
@@ -0,0 +1,57 @@
+carLogApp.factory('Authentication', function($firebase,
+	$firebaseAuth, $rootScope, $routeParams, $location, FIREBASE_URL){
+		var ref = new Firebase(FIREBASE_URL);
+		var auth = $firebaseAuth(ref);
+		
+		auth.$onAuth(function(authUser){
+			if(authUser){
+				var ref = new Firebase(FIREBASE_URL + 'users/' + authUser.uid);
+				var user = $firebase(ref).$asObject();
+				$rootScope.currentUser = user;
+			}else{
+				$rootScope.currentUser = '';
+			};
+		});
+		
+		var myObject = {
+			login: function(user){
+				console.log("authentication factory login");
+				return auth.$authWithPassword({
+					email:user.email,
+					password: user.password
+				});//authWithPassword
+			},//Login
+			
+			logout: function(user){
+				return auth.$unauth();
+			},//LogOut
+			
+			register: function(user){
+				return auth.$createUser({
+					email: user.email,
+					password: user.password
+				}).then(function(regUesr){
+					var ref = new Firebase(FIREBASE_URL + 'users/');
+					var firebaseUsers = $firebase(ref);
+					
+					var userInfo = {
+						date : Firebase.ServerValue.TIMESTAMP,
+						regUser : regUser.uid,
+						firstname : user.firstname,
+						email : user.email
+					};//user info
+					
+					firebaseUsers.$set(regUser.uid, userInfo);
+				});
+			},
+			
+			requireAuth: function() {
+				return auth.$requireAuth(); 
+			},//require Authentication
+			
+			waitForAuth: function() {
+				return auth.$waitForAuth(); 
+			} //Wait until user is Authenticated
+		};
+		return myObject;
+	});
