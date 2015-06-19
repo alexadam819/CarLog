e29bc19d4e37b6d94dc0fe6ab85cde2a35652bbe
Alexander Adam <alexadam@gmail.com>
2015-06-19 00:07:14 -0400

    Working on the car registration form.

--- js/controllers/garage.js
+++ js/controllers/garage.js
@@ -5,5 +5,9 @@ carLogApp.controller('GarageController',
 			console.log("Add Car");
 			$location.path('/garage/addcar');
 		};//Takes user the add car form
+		
+		$scope.addCarForm = function(){
+			console.log($scope.car);
+		};//Submitting the car registration form
 	
 });
